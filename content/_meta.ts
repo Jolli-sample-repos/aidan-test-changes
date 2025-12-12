a0301f4d5b2e50a79524b13d129a0a7cb3ef7532
Joe Hart <joe@jeverest.com>
2025-12-12 15:07:45 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,4 +1,12 @@
 export default {
   'index': { display: 'hidden' },
-  'troubleshooting': 'Troubleshooting'
+  'troubleshooting': 'Troubleshooting',
+  'api-reference': {
+    title: 'API Reference',
+    type: 'menu',
+    items: {
+      'petstore-api': { title: 'Petstore API', href: '/api-docs/petstore-api' },
+      'jolli-api': { title: 'jolli-api', href: '/api-docs/jolli-api' }
+    }
+  }
 }
