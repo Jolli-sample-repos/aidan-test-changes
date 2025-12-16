6d15c57b96c9d455d14acea19b57bf1ee4ea9106
Joe Hart <joe@jeverest.com>
2025-12-16 11:27:13 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -5,7 +5,7 @@ export default {
     title: 'Custom Menu Title Text',
     type: 'menu',
     items: {
-      'petstore-api': { title: 'Custom OpenAPI Text 1', href: '/api-docs/petstore-api' },
+ 'petstore-api': { title: 'Custom OpenAPI Text 1', href: '/api-docs/petstore-api' },
       'jolli-api': { title: 'Custom OpenAPI Text 2', href: '/api-docs/jolli-api' }
     }
   }
